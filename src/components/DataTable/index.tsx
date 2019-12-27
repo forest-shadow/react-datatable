f58483745e1fbb281173bc8830e1d7e22c095755
forest-shadow <mamasay@yandex.ru>
2019-12-27 16:30:33 +0300

    Types: extract Datatable pagination interface

--- src/components/DataTable/index.tsx
+++ src/components/DataTable/index.tsx
@@ -2,20 +2,16 @@ import React from 'react';
 
 import './style.css';
 import { IDatatableHeader } from './interfaces/DataTableHeader';
+import { IDataTablePagination } from "./interfaces/DataTablePagination";
 
 const DataTable: React.FC<Props> = () => {
   return <div>DataTable component</div>;
 };
 
-interface Pagination {
-  pageLength: number;
-  extended: boolean;
-}
-
 interface Props {
   title: string;
   keyField: string;
-  pagination: Pagination;
+  pagination: IDataTablePagination;
   headers: IDatatableHeader[];
   data: Array<{ [indes: string]: any }>;
   noDataMessage: string;
---
+++ src/components/DataTable/interfaces/DataTablePagination.ts
@@ -0,0 +1,4 @@
+export interface IDataTablePagination {
+  pageLength: number;
+  extended: boolean;
+}
